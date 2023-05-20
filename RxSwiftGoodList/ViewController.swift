f844f04b7eb9e2f3b832eaba5ccda12ba5d98bb9
Daniel Karath <karath.dani@gmail.com>
2023-05-20 14:36:35 +0200

    Initial Commit

---
+++ RxSwiftGoodList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftGoodList
+//
+//  Created by Daniel Karath on 5/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
